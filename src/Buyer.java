99327b5b089dae2eb4794d157596ba9a4d91e642
Haidir Abdillah <101625256+citer98@users.noreply.github.com>
2023-01-08 02:22:21 +0700

    Update Buyer.java

--- src/Buyer.java
+++ src/Buyer.java
@@ -1,27 +1,21 @@
-public class Buyer implements User{
-    int opsi;
-    private boolean isAdd();
+public class Buyer {
+    private User user;
 
-    @Override
-    public void ShowGameList()
-    {
-        System.out.println("\t\t Daftar Game \t\t");
-        System.out.println("=======================");
-        System.out.println("No  : 1\n");
-        System.out.println("Nama: Satu\n");
-        System.out.println("Sinopsis: Sinopsis satu\n");
-        System.out.println("Harga   : Harga satu\n\n");
-        \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-        System.out.println("No  : 2\n");
-        System.out.println("Nama: Dua\n");
-        System.out.println("Sinopsis: Sinopsis Dua\n");
-        System.out.println("Harga   : Harga Dua\n\n");
-        System.out.println("=======================");
+    public Buyer(User user){
+        this.user = user;
+    }
+    void GameList(){
+        this.user.ShowGameList();
+    }
+    void Add(){
+        this.user.AddToCart();
+    }
+    void Remove(){
+        this.user.RemoveFromCart();
+    }
+    void Pay(){
+        this.user.CheckOut();
     }
 
-    public void AddToCart()
-    {
-        System.out.println("Masukan nomor game yang akan di beli\n");
-        System.out.println("Pilihan : ");
-    }
 }
+
